08a1ed3fe8bd0eb95098da6de404f807ce683004
IAntonioM <isaiasmayhuaym@gmail.com>
2025-06-16 22:34:26 -0500

    data

--- resources/views/livewire/recibido/recibido-component.blade.php
+++ resources/views/livewire/recibido/recibido-component.blade.php
@@ -82,12 +82,12 @@
                                                 <span class="path2"></span>
                                             </i>
                                         </a>
-                                        <a href="#" class="btn btn-sm btn-icon btn-light-danger"
+                                        {{-- <a href="#" class="btn btn-sm btn-icon btn-light-danger"
                                         data-bs-toggle="tooltip" data-bs-placement="top" title="Mandar a Papelera" >
                                             <i class="ki-duotone ki-trash fs-2"><span class="path1"></span><span
                                                     class="path2"></span><span class="path3"></span><span
                                                     class="path4"></span><span class="path5"></span></i>
-                                        </a>
+                                        </a> --}}
                                     </div>
                                 </div>
                             </td>
